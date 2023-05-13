58bf23528971f62c96c4f8bcb55f1ab92b4757cc
Ibraimov23 <ibraimovnursultan23@gmail.com>
2023-05-13 14:57:56 +0600

    updated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,12 @@
 import React from "react"
 import AOS from 'aos';
 import "aos/dist/aos.css"
+
+export const onInitialClientRender = () => {
+    setTimeout(function() {
+        document.getElementById("___loader").style.display = "none"
+    }, 1000)
+}
 export const onClientEntry = () => {
     AOS.init()
-  }
+}
