76527a0a9d4c5b60249399d05f5015b4b08f5b1d
Ibraimov23 <ibraimovnursultan23@gmail.com>
2023-05-16 00:25:06 +0600

    updated

--- src/components/lending/faq-page/faq-card.js
+++ src/components/lending/faq-page/faq-card.js
@@ -1,10 +1,23 @@
-import * as React from "react"
+import React, { useState } from "react";
+import { Fade } from "react-reveal";
+import Typist from 'react-typist';
+import { useInView } from "react-intersection-observer";
 
 const Card = (props) => {
+    const [isVisible, setIsVisible] = useState(false);
+	const { ref, inView } = useInView({ threshold: 0 });
+	const handleTypingDone = () => {
+		console.log("");
+	};
+	const startTyping = () => {
+		setIsVisible(true);
+	};
     return (
-        <div class="faq-page__description" data-aos="fade-up">
-            <p class="faq-page__description_text faq-page__description_text1">{props.question}</p>
-            <p class="faq-page__description_text faq-page__description_text2">{props.answer}</p>
+        <div class="faq-page__description" ref={ref}>
+            <p class="faq-page__description_text faq-page__description_text1"><Fade duration={2000}>{props.question}</Fade></p>
+            <p class="faq-page__description_text faq-page__description_text2">{inView && (
+                <Typist avgTypingDelay={10} onTypingDone={handleTypingDone}
+                    startDelay={100} cursor={{ show: false }}>{props.answer}</Typist>)}</p>
         </div>
     )
 }
--- src/components/lending/tokenomic-page/tokenomic.js
+++ src/components/lending/tokenomic-page/tokenomic.js
@@ -1,18 +1,29 @@
 import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+import { Slide } from "react-reveal"
 
 export const Tokenomic = ({ siteTitle }) => {
 
 	return (
 		<div class="tokenomic-page" id="tokenomic">
-			<div class="tokenomic-page__limit"></div>
-			<div class="tokenomic-page__container _container">
-				<h1 class="tokenomic-page__header header-text" data-aos="fade-up">TOKENOMICS</h1>
-				<div class="tokenomic-page__descriptions">
-					<h5 class="tokenomic-page__descriptions_head" data-aos="fade-up">Token supply - 199 124 800 000 000</h5>
-					<div class="descriptions__texts" data-aos="fade-up">
-						<p class="descriptions__texts_text">The whole supply is generated at the token launch and is locked into the Uniswap v2 liquidity via Uncx for 2 years.</p>
-						<p class="descriptions__texts_text">The tax profits are accumulated on the $NAFO treasury and periodically sent to the official charity addresses. The $NAFO team will provide transaction proofs for every donation.</p>
-						<p class="descriptions__texts_text">The $NAFO token also has an anti-whale protection, which limits all transactions (including buying or selling) to 1% of the total supply.</p>
+			<div class="tokenomic-page__line"></div>
+			<div class="tokenomic-page__container _container" data-aos-duration="1000">
+				<div class="tokenomic-page__row">
+					<div class="row__image">
+						<Slide left duration={2000}><StaticImage src="../../../images/tokenomic/info.svg" alt="info" placeholder="blurred" /></Slide>
+					</div>
+					<div class="row__description">
+						<h1 class="tokenomic-page__header header-text" data-aos="fade-up">TOKENOMICS</h1>
+						<Slide right duration={2000}>
+							<div>
+								<h5 class="row__description_head">Token supply - 199 124 800 000 000</h5>
+								<div class="row__descriptions">
+									<p class="row__descriptions_text">The whole supply is generated at the token launch and is locked into the Uniswap v2 liquidity via <a href="https://uncx.network/" class="_line">Uncx</a> for 2 years.</p>
+									<p class="row__descriptions_text">The tax profits are accumulated on the $NAFO treasury and periodically sent to the official charity addresses. The $NAFO team will provide transaction proofs for every donation.</p>
+									<p class="row__descriptions_text">The $NAFO token also has an anti-whale protection, which limits all transactions (including buying or selling) to 1% of the total supply.</p>
+								</div>
+							</div>
+						</Slide>
 					</div>
 				</div>
 			</div>
