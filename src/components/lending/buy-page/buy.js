8627b78142e1a7617a39458eb660f8a57fdfea19
Ibraimov23 <ibraimovnursultan23@gmail.com>
2023-05-21 18:20:20 +0600

    updated

--- src/components/lending/buy-page/buy.js
+++ src/components/lending/buy-page/buy.js
@@ -6,12 +6,12 @@ import Icon from '../../icons/icon';
 import { Video } from "../../utils";
 
 export const Buy = ({ siteTitle }) => {
-	const { ref, inView } = useInView({ threshold: 0 });
+	/*const { ref, inView } = useInView({ threshold: 0 });
 	const [isTypingComplete, setIsTypingComplete] = useState(false);
 
 	const handleTypingDone = () => {
 		setIsTypingComplete(true);
-	};
+	};*/
 
 	return (
 		<div class="buy-page" id="buy">
@@ -29,10 +29,11 @@ export const Buy = ({ siteTitle }) => {
 							</div>
 							<div class="row__description">
 								<p class="row__description_header"><Fade duration={2000}>Create a Wallet</Fade></p>
-								<p class="row__description_desc"> {inView && !isTypingComplete ? (
+								<p class="row__description_desc"> {/*inView && !isTypingComplete ? (
 									<Typist avgTypingDelay={15} onTypingDone={handleTypingDone}
 										startDelay={100} cursor={{ show: false }}>If you are using a desktop, we suggest visiting <a href="https://metamask.io/" class="_line"> Metamask.io</a> and downloading their browser extension. For mobile, install any Ethereum wallet that can be connected to Uniswap.
-									</Typist>) : (<>If you are using a desktop, we suggest visiting <a href="https://metamask.io/" class="_line"> Metamask.io</a> and downloading their browser extension. For mobile, install any Ethereum wallet that can be connected to Uniswap</>)}
+								</Typist>) : (<>If you are using a desktop, we suggest visiting <a href="https://metamask.io/" class="_line"> Metamask.io</a> and downloading their browser extension. For mobile, install any Ethereum wallet that can be connected to Uniswap</>)*/}
+								If you are using a desktop, we suggest visiting <a href="https://metamask.io/" class="_line"> Metamask.io</a> and downloading their browser extension. For mobile, install any Ethereum wallet that can be connected to Uniswap
 								</p>
 							</div>
 						</div>
@@ -49,11 +50,12 @@ export const Buy = ({ siteTitle }) => {
 							<Fade duration={2000}>
 								<div class="row__description">
 									<p class="row__description_header">Get Some ETH</p>
-									<p class="row__description_desc"> {inView && !isTypingComplete ? (
+									<p class="row__description_desc"> {/*inView && !isTypingComplete ? (
 										<Typist avgTypingDelay={15} onTypingDone={handleTypingDone}
 											startDelay={100} cursor={{ show: false }}>
 											You will need ETH to pay for transaction fees when buying $NAFO. You can buy some ETH directly in Metamask or just get it transferred to your wallet from an exchange or your fellas.
-										</Typist>) : (<>You will need ETH to pay for transaction fees when buying $NAFO. You can buy some ETH directly in Metamask or just get it transferred to your wallet from an exchange or your fellas.</>)}
+									</Typist>) : (<>You will need ETH to pay for transaction fees when buying $NAFO. You can buy some ETH directly in Metamask or just get it transferred to your wallet from an exchange or your fellas.</>)*/}
+									You will need ETH to pay for transaction fees when buying $NAFO. You can buy some ETH directly in Metamask or just get it transferred to your wallet from an exchange or your fellas.
 									</p>
 								</div>
 							</Fade>
@@ -71,11 +73,12 @@ export const Buy = ({ siteTitle }) => {
 							<Fade duration={2000}>
 								<div class="row__description">
 									<p class="row__description_header">Go to Uniswap</p>
-									<p class="row__description_desc"> {inView && !isTypingComplete ? (
+									<p class="row__description_desc"> {/*inView && !isTypingComplete ? (
 										<Typist avgTypingDelay={15} onTypingDone={handleTypingDone}
 											startDelay={100} cursor={{ show: false }}>
 											Open the <a href="https://app.uniswap.org/" class="_line">Uniswap app</a>, connect your wallet, and paste the $NAFO smart contract address. Or simply visit <a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f" class="_line">this link</a> for a pre-configured setup with $NAFO as received currency.
-										</Typist>) : (<>Open the <a href="https://app.uniswap.org/" class="_line">Uniswap app</a>, connect your wallet, and paste the $NAFO smart contract address. Or simply visit <a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f" class="_line">this link</a> for a pre-configured setup with $NAFO as received currency.</>)}
+									</Typist>) : (<>Open the <a href="https://app.uniswap.org/" class="_line">Uniswap app</a>, connect your wallet, and paste the $NAFO smart contract address. Or simply visit <a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f" class="_line">this link</a> for a pre-configured setup with $NAFO as received currency.</>)*/}
+									Open the <a href="https://app.uniswap.org/" class="_line">Uniswap app</a>, connect your wallet, and paste the $NAFO smart contract address. Or simply visit <a href="https://app.uniswap.org/#/swap?outputCurrency=0xaf59f6613920d98ef64a133b21b28495a63e808f" class="_line">this link</a> for a pre-configured setup with $NAFO as received currency.
 									</p>
 								</div>
 							</Fade>
@@ -93,12 +96,13 @@ export const Buy = ({ siteTitle }) => {
 							<Fade duration={2000}>
 								<div class="row__description">
 									<p class="row__description_header">Become a fella</p>
-									<p class="row__description_desc"> {inView && !isTypingComplete ? (
+									<p class="row__description_desc"> {/*inView && !isTypingComplete ? (
 										<Typist avgTypingDelay={15} onTypingDone={handleTypingDone} startDelay={100} cursor={{ show: false }}>
 											Time to join $NAFO! Set the <a href="https://support.uniswap.org/hc/en-us/articles/8643879653261-What-is-Price-Slippage-"
 												class="_line">slippage</a> to at least 4-5%, enter how much $NAFO you want to buy, and press Swap. Pay the gas fee and wait for your transaction to be confirmed. Congratulations, you’re a fella now!
 										</Typist>) : (<>Time to join $NAFO! Set the <a href="https://support.uniswap.org/hc/en-us/articles/8643879653261-What-is-Price-Slippage-"
-											class="_line">slippage</a> to at least 4-5%, enter how much $NAFO you want to buy, and press Swap. Pay the gas fee and wait for your transaction to be confirmed. Congratulations, you’re a fella now!</>)}
+									class="_line">slippage</a> to at least 4-5%, enter how much $NAFO you want to buy, and press Swap. Pay the gas fee and wait for your transaction to be confirmed. Congratulations, you’re a fella now!</>)*/}Time to join $NAFO! Set the <a href="https://support.uniswap.org/hc/en-us/articles/8643879653261-What-is-Price-Slippage-"
+											class="_line">slippage</a> to at least 4-5%, enter how much $NAFO you want to buy, and press Swap. Pay the gas fee and wait for your transaction to be confirmed. Congratulations, you’re a fella now!
 									</p>
 								</div>
 							</Fade>
--- src/components/lending/faq-page/faq-card.js
+++ src/components/lending/faq-page/faq-card.js
@@ -6,9 +6,9 @@ const Card = (props) => {
     return (
         <div class="faq-page__description">
             <p class="faq-page__description_text faq-page__description_text1"><Fade duration={2000}>{props.question}</Fade></p>
-            <p class="faq-page__description_text faq-page__description_text2">{props.inView && !props.isTypingComplete ? (
+            <p class="faq-page__description_text faq-page__description_text2">{/*props.inView && !props.isTypingComplete ? (
                 <Typist avgTypingDelay={10} onTypingDone={props.handleTypingDone}
-                    startDelay={100} cursor={{ show: false }}>{props.answer}</Typist>):(<>{props.answer}</>)}</p>
+            startDelay={100} cursor={{ show: false }}>{props.answer}</Typist>):(<>{props.answer}</>)*/}{props.answer}</p>
         </div>
     )
 }
